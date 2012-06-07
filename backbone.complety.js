3039a9f0dcb767bb127bfc1778e0823f600c9f6a
shadowrider <myroslav.pom@gmail.com>
2012-06-06 23:16:56 -0700

    autocomplete fully works now on input text field

--- backbone.complety.js
+++ backbone.complety.js
@@ -38,7 +38,7 @@
     tagName: 'div',
 
     close: function() {
-      this._targetContainer.off();
+      this._$targetContainer.off();
       this.remove();
       this.unbind();
     },
@@ -55,10 +55,11 @@
       if(!options.ignoreCase) {
         this.ignoreCase = true;
       }
-      this._targetContainer = $(options.targetContainer);
-      this._targetContainer.addClass('complity-container');
+      this._$targetContainer = $(options.targetContainer);
+      this._$targetContainer.addClass('complity-container');
       this.isArea = options.isArea;
-      this.container = new Backbone.Complety.Container({ attr: this.searchAttr });
+      this._container = new Backbone.Complety.Container({ attr: this.searchAttr });
+      this._container.on('setSelected', this._setSelected, this);
       this.render();
     },
 
@@ -68,7 +69,10 @@
         inputTag = '<textarea rows="10" cols="35" class="field"></textarea>';
       }
       this.$el.html(inputTag);
-      this._targetContainer.append(this.el);
+      this._$targetContainer.append(this.el);
+//      if(this.isArea) {
+//        this._createAreaDiv();
+//      }
 
       return this;
     },
@@ -77,64 +81,72 @@
       event.preventDefault();
       //Enter
       if(event.keyCode === 13 || event.keyCode === 9) {
-        this.container.remove();
+        this._container.remove();
         this._updateInput();
         return false;
       }
       //ESC
       if (event.keyCode === 27) {
-        this.container.remove();
+        this._selected = null;
+        this._container.remove();
         return false;
       }
       //DOWN
       if (event.keyCode === 40) {
-        this.selected = this.container.selectLowerItem();
+        this._selected = this._container.selectLowerItem();
         this._updateInput();
         return false;
       }
       //UP
       if (event.keyCode === 38) {
-        this.selected = this.container.selectHigherItem();
+        this._selected = this._container.selectHigherItem();
         this._updateInput();
         return false;
       } else {
         this._renderComplety();
+        if(this.isArea) {
+          this._wrapWords(event);
+        }
       }
     },
 
+    _setSelected: function(selected) {
+      this._selected = selected;
+      this._updateInput();
+    },
+
     _updateInput: function() {
-      this.$('.field').val(this.selected.get(this.searchAttr));
+      this.$('.field').val(this._selected.get(this.searchAttr));
     },
 
     _renderComplety: function() {
       var results = this._checkCollection();
-      if(this.container)
-        this.container.remove();
+      if(this._container) {
+        this._container.remove();
+      }
       if(!_.isEmpty(results)) {
-        this.container.updateResults(results);
-        this.$el.append(this.container.render().el);
+        this._container._updateResults(results);
+        this.$el.append(this._container.render().el);
       }
     },
 
     _checkCollection: function() {
-      var self = this;
-      var searchStr = this.$('.field').val().trim();
-      var searchStcLC = searchStr.toLowerCase();
-      // Find matches
-      var results = [];
+      var self = this,
+        searchStr = this.$('.field').val().trim(),
+        searchStcLC = searchStr.toLowerCase(),
+        results = [];
       if(!searchStr) {
         return results;
       }
       if(searchStr.length > 2) {
         this.collection.each(function(model) {
           var value = model.get(self.searchAttr);
-          if ((value.indexOf(searchStr) !== -1 && value !== searchStr)
-            || (self.ignoreCase && value.toLowerCase().indexOf(searchStcLC) !== -1 && value.toLowerCase() !== searchStcLC)) {
+          if ((value.indexOf(searchStr) !== -1 && value !== searchStr) ||
+            (self.ignoreCase && value.toLowerCase().indexOf(searchStcLC) !== -1 && value.toLowerCase() !== searchStcLC)) {
             results.push(model);
           }
         });
       }
-      console.log(results);
       return results;
     }
 
@@ -146,57 +158,75 @@
 
     initialize: function(options) {
       this.searchAttr = options.attr;
-      this.listPointer = -1;
+      this._listPointer = -1;
     },
 
     render: function() {
       this.resultItems = [];
       this.$el.html(" ");
       _.each(this.results, function(item) {
-        var item = new Backbone.Complety.Item({ model: item, searchAttr: this.searchAttr });
-        this.resultItems.push(item);
-        this.$el.append(item.render().el);
+        var completyItem = new Backbone.Complety.Item({ model: item, searchAttr: this.searchAttr });
+        completyItem.on('deselectItems', this._deselectItems, this);
+        completyItem.on('setItem', this._passSelected, this);
+        this.resultItems.push(completyItem);
+        this.$el.append(completyItem.render().el);
       }, this);
 
       return this;
     },
 
-    updateResults: function(results) {
+    _updateResults: function(results) {
       this.results = results;
     },
 
+    _passSelected: function(selected) {
+      this.trigger('setSelected', selected);
+      this.remove();
+    },
+
     selectLowerItem: function() {
-      this.listPointer++;
-      if(this.listPointer === this.resultItems.length) {
-        this.listPointer = 0;
+      this._listPointer++;
+      if(this._listPointer === this.resultItems.length) {
+        this._listPointer = 0;
       }
-      if(this.listPointer === 0) {
-        this.resultItems[this.resultItems.length-1].$el.removeClass('selected');
+      if(this._listPointer === 0) {
+        this.resultItems[this.resultItems.length-1].deselect();
       } else {
-        this.resultItems[this.listPointer-1].$el.removeClass('selected');
+        this.resultItems[this._listPointer-1].deselect();
       }
-      var result = this.resultItems[this.listPointer];
-      result.$el.addClass('selected');
+      var result = this.resultItems[this._listPointer];
+      result.select();
       return result.model;
     },
 
     selectHigherItem: function() {
-      this.listPointer--;
-      if(this.listPointer < 0) {
-        this.listPointer = this.resultItems.length-1;
-        this.resultItems[0].$el.removeClass('selected');
+      this._listPointer--;
+      if(this._listPointer < 0) {
+        this._listPointer = this.resultItems.length-1;
+        this.resultItems[0].deselect();
       } else {
-        this.resultItems[this.listPointer+1].$el.removeClass('selected');
+        this.resultItems[this._listPointer+1].deselect();
       }
-      var result = this.resultItems[this.listPointer];
-      result.$el.addClass('selected');
+      var result = this.resultItems[this._listPointer];
+      result.select();
       return result.model;
+    },
+
+    _deselectItems: function() {
+      this._listPointer = -1;
+      _.invoke(this.resultItems, 'deselect');
     }
   });
 
   Backbone.Complety.Item = Backbone.View.extend({
     className: 'result',
     tagName: 'li',
+
+    events: {
+      'mouseover':  'select',
+      'mouseleave': 'deselect',
+      'click':      '_setItem'
+    },
 
     initialize: function(options) {
       this.searchAttr = options.searchAttr;
@@ -206,6 +236,19 @@
       this.$el.html(_.template('<span><%= content %></span>',{ content: this.model.get(this.searchAttr) }));
 
       return this;
+    },
+
+    select: function() {
+      this.trigger('deselectItems');
+      this.$el.addClass('selected');
+    },
+
+    deselect: function() {
+      this.$el.removeClass('selected');
+    },
+
+    _setItem: function() {
+      this.trigger('setItem', this.model);
     }
   });
 
